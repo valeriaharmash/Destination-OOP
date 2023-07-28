efb096d270a494edc027f4a2481b24634eee2790
CATT_CODE <65243972+CATT-CODE@users.noreply.github.com>
2023-07-28 12:55:57 -0700

    Person and Bag done

--- __tests__/classes/Person.test.js
+++ __tests__/classes/Person.test.js
@@ -7,30 +7,30 @@ describe("Person", () => {
 
 		expect(person.name).toBe("John");
 		expect(person.destination).toBe("JFK");
-	})
+	});
 
 	it("should have bags that initialize with an empty array", () => {
-		const person = new Person("John", "JFK")
+		const person = new Person("John", "JFK");
 
 		expect(person.bags).toEqual([]);
-	})
+	});
 
-	it('should add a bag to the owners bag array', () => {
-		const person = new Person("John", "JFK")
-		const bag = new Bag(40, 2)
+	it("should add a bag to the owners bag array", () => {
+		const person = new Person("John", "JFK");
+		const bag = new Bag(40, 2);
 
-		person.addBag(bag)
+		person.addBag(bag);
 
-		expect(person.bags).toContain(bag)
-	})
+		expect(person.bags).toContain(bag);
+	});
 
 	it("should return the array of bags assigned to the owner", () => {
-		const person = new Person("John", "JFK")
-		const bag = new Bag(40, 2)
+		const person = new Person("John", "JFK");
+		const bag = new Bag(40, 2);
 
-		person.addBag(bag)
-		ownersBags = person.getBags(bag)
+		person.addBag(bag);
+		ownersBags = person.getBags(bag);
 
-		expect(ownersBags).toContain(bag)
-	})
-})
+		expect(ownersBags).toContain(bag);
+	});
+});
