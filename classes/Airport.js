6663e5c01b8b3b5ea0f6f2a1f517334c7e92e7de
Valeriia Harmash <valeriaharmash@gmail.com>
2023-07-28 16:36:34 -0400

    Plane, Airport done

--- classes/Airport.js
+++ classes/Airport.js
@@ -1,7 +1,13 @@
 class Airport {
+	static airportCode = 'LAX'
+	constructor(name) {
+		this.name = name
+		this.planes = []
+	}
 
-
+	addPlane(plane) {
+		this.planes.push(plane)
+	}
 }
 
-
-module.exports = Airport;
+module.exports = Airport
--- classes/Plane.js
+++ classes/Plane.js
@@ -1,7 +1,17 @@
 class Plane {
+	constructor(company, origin, destination) {
+		this.company = company
+		this.origin = origin
+		this.destination = destination
+		this.passengers = []
+	}
+	addPassenger(passenger) {
+		this.passengers.push(passenger)
+	}
 
-
+	getPassengers() {
+		return this.passengers
+	}
 }
 
-
-module.exports = Plane;
+module.exports = Plane
